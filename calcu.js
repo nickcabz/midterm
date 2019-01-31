7cf76848d8305c39514e21ad3915c900394eff74
nickcabz <45448249+nickcabz@users.noreply.github.com>
2019-01-31 16:27:13 +0800

    Update calcu.js

--- calcu.js
+++ calcu.js
@@ -7,33 +7,33 @@ function ud(n){
 
 function ans(){
 	c=eval(disp.innerHTML);
-	disp.innerhtml=c;
+	disp.innerHTML=c;
 }
 
 function clc(){
 disp.innerHTML='';
 }
 
-document.getElementsById('1').addEventListener("click", function() { ud(1); });
-document.getElementsById('2').addEventListener("click", function() { ud(2); });
-document.getElementsById('3').addEventListener("click", function() { ud(3); });
-document.getElementsById('4').addEventListener("click", function() { ud(4); });
-document.getElementsById('5').addEventListener("click", function() { ud(5); });
-document.getElementsById('6').addEventListener("click", function() { ud(6); });
-document.getElementsById('7').addEventListener("click", function() { ud(7); });
-document.getElementsById('8').addEventListener("click", function() { ud(8); });
-document.getElementsById('9').addEventListener("click", function() { ud(9); });
-document.getElementsById('0').addEventListener("click", function() { ud(0); });
+document.getElementById('1').addEventListener("click", function() { ud(1); });
+document.getElementById('2').addEventListener("click", function() { ud(2); });
+document.getElementById('3').addEventListener("click", function() { ud(3); });
+document.getElementById('4').addEventListener("click", function() { ud(4); });
+document.getElementById('5').addEventListener("click", function() { ud(5); });
+document.getElementById('6').addEventListener("click", function() { ud(6); });
+document.getElementById('7').addEventListener("click", function() { ud(7); });
+document.getElementById('8').addEventListener("click", function() { ud(8); });
+document.getElementById('9').addEventListener("click", function() { ud(9); });
+document.getElementById('0').addEventListener("click", function() { ud(0); });
 
-document.getElementsById('dec').addEventListener("click", function() { ud('.');});
+document.getElementById('dec').addEventListener("click", function() { ud('.');});
 
-document.getElementsById('add').addEventListener("click", function() { ud('+');});
-document.getElementsById('subt').addEventListener("click", function() { ud('-');});
-document.getElementsById('multi').addEventListener("click", function() { ud('*');});
-document.getElementsById('div').addEventListener("click", function() { ud('/');});
+document.getElementById('add').addEventListener("click", function() { ud('+');});
+document.getElementById('subt').addEventListener("click", function() { ud('-');});
+document.getElementById('multi').addEventListener("click", function() { ud('*');});
+document.getElemenById('div').addEventListener("click", function() { ud('/');});
 
-document.getElementsById('clear').addEventListener("click", function() { clc();});
-document.getElementsById('ans').addEventListener("click", function() { ans();});
+document.getElementById('clear').addEventListener("click", function() { clc();});
+document.getElementById('ans').addEventListener("click", function() { ans();});
 
 
 
